2a606606db270bfb26da72b67e33ff39bcc3d168
pragyasukrity007 <pragyasukrity2712@gmail.com>
2024-04-25 11:02:29 +0530

    ticket app

---
+++ app/(components)/TicketForm.jsx
@@ -0,0 +1,184 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const TicketForm = ({ ticket }) => {
+  const EDITMODE = ticket._id === "new" ? false : true;
+
+  const router = useRouter();
+
+  const startingTicketData = {
+    title: "",
+    description: "",
+    category: "Software Request",
+    priority: 1,
+    progress: 0,
+    status: "Not Started",
+  };
+
+  if (EDITMODE) {
+    startingTicketData["title"] = ticket.title;
+    startingTicketData["description"] = ticket.description;
+    startingTicketData["category"] = ticket.category;
+    startingTicketData["priority"] = ticket.priority;
+    startingTicketData["progress"] = ticket.progress;
+    startingTicketData["status"] = ticket.status;
+  }
+
+  const [formData, setFormData] = useState(startingTicketData);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFormData((preState) => ({
+      ...preState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (EDITMODE) {
+      const res = await fetch(`/api/Tickets/${ticket._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ formData }),
+      });
+      if (!res.ok) {
+        throw new Error("Failed to update ticket");
+      }
+    } else {
+      const res = await fetch("/api/Tickets", {
+        method: "POST",
+        body: JSON.stringify({ formData }),
+        //@ts-ignore
+        "Content-Type": "application/json",
+      });
+      if (!res.ok) {
+        throw new Error("Failed to create ticket");
+      }
+    }
+
+    router.refresh();
+    router.push("/");
+  };
+
+  return (
+    <div className="flex justify-center">
+      <form
+        className="flex flex-col gap-3 w-1/2"
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <h3>{EDITMODE ? "Update Your Ticket" : "Create your Ticket"}</h3>
+        <label>Title</label>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          value={formData.title}
+          onChange={handleChange}
+          required={true}
+        />
+        <label>Description</label>
+        <textarea
+          id="description"
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+          required={true}
+          rows="5"
+        />
+
+        <label>Category</label>
+        <select
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+        >
+          <option value="Software Request">Software Request</option>
+          <option value="Hardware Problem">Hardware Problem</option>
+          <option value="Project">Project</option>
+          <option value="IT Clinic Appointment">IT Clinic Appointment</option>
+        </select>
+
+        <label>Priority</label>
+        <div>
+          <input
+            id="priority-1"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={1}
+            checked={formData.priority == 1}
+          />
+          <label>1</label>
+          <input
+            id="priority-2"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={2}
+            checked={formData.priority == 2}
+          />
+          <label>2</label>
+          <input
+            id="priority-3"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={3}
+            checked={formData.priority == 3}
+          />
+          <label>3</label>
+          <input
+            id="priority-4"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={4}
+            checked={formData.priority == 4}
+          />
+          <label>4</label>
+          <input
+            id="priority-5"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={5}
+            checked={formData.priority == 5}
+          />
+          <label>5</label>
+        </div>
+
+        <label>Progress</label>
+        <input
+          type="range"
+          name="progress"
+          value={formData.progress}
+          min="0"
+          max="100"
+          onChange={handleChange}
+        />
+        <label>Status</label>
+        <select name="status" value={formData.status} onChange={handleChange}>
+          <option value="not started">Not Started</option>
+          <option value="started">Started</option>
+          <option value="done">Done</option>
+        </select>
+        <input
+          type="submit"
+          className="button"
+          value={EDITMODE ? "Update Ticket" : "Create Ticket"}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default TicketForm;
---
+++ app/api/Tickets/route.js
@@ -0,0 +1,46 @@
+import Ticket from "../../(models)/Ticket";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  console.log("POSTTTTTTTTTTT");
+  try {
+    const body = await req.json();
+    const ticketData = body.formData;
+    await Ticket.create(ticketData);
+    return NextResponse.json(
+      {
+        message: "Ticket Created",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "ERROR",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    const tickets = await Ticket.find();
+    return NextResponse.json({ tickets }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "ERROR",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
